533790de578d8d3c6c4199490120d8020d5d1256
NeuralNexusLab <neuralnexuslab@hotmail.com>
2025-11-25 06:05:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -462,6 +462,7 @@ app.post('/api/models', requireUserAuth, async (req, res) => {
             });
             const data = await apiRes.json();
             reply = data?.output?.[1]?.content?.[0]?.text || "[No Content]";
+            console.log("OpenAI Raw Res: " + JSON.stringify(data));
         }
 
         // Save Chat
